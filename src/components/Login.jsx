d027110f24427faf2ed182238bcb7bfdb75ee289
Pavas Garg <pavasgarg2003@gmail.com>
2022-08-07 18:24:01 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,16 +34,19 @@ export default function Login(props) {
     };
     return (
         <>
-                <div className="container" id="logincon">
+                   <div className="container" id="logincon">
+                    <span id="loginimg">
+                    <img src = "./cloud-icon.png"/>
                     <h2>Login to use CloudNote</h2>
+                    </span>
                     <form onSubmit={handlesubmit} autoComplete="off" id="loginform">
                         <div className="mb-0">
-                            <label htmlFor="exampleInputEmail1" className="form-label">Email Address</label><br />
-                            <input type="email" className="logininput" name="email" required id="exampleInputEmail1" autoComplete="off" aria-describedby="emailHelp" value={credentials.email} onChange={onchange} />
+                            {/* <label htmlFor="exampleInputEmail1" className="form-label">Email Address</label><br /> */}
+                            <input type="email" className="logininput" placeholder="Email Address" name="email" required id="exampleInputEmail1" autoComplete="off" aria-describedby="emailHelp" value={credentials.email} onChange={onchange} />
                         </div>
                         <div className="mb-0">
-                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label><br />
-                            <input type="password"   className="logininput" required value={credentials.password} name="password" autoComplete="off" id="exampleInputPassword1" onChange={onchange} />
+                            {/* <label htmlFor="exampleInputPassword1" className="form-label">Password</label><br /> */}
+                            <input type="password"   className="logininput" placeholder="Password" required value={credentials.password} name="password" autoComplete="off" id="exampleInputPassword1" onChange={onchange} />
                         </div>
                         <button type="submit" id="loginbtn" className="btn btn-primary" >Login</button>
                     </form>
