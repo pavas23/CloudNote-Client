2c48be39eee14644b6f1788897ddb458e4129438
Pavas Garg <pavasgarg2003@gmail.com>
2022-08-08 12:49:50 +0530

    added placeholder

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -12,9 +12,6 @@ export default function AddNote() {
         tag:""
     });
     const handleclick = (event)=>{
-        if(note.title.length==0 || note.description.length==0){
-            return alert("Title or description can not be blank");
-        }
         event.preventDefault();
         addNote(note.title,note.description,note.tag);
         document.getElementById("tag").value = "";
@@ -33,20 +30,20 @@ export default function AddNote() {
         <>
             <div className="container" id="addnotecon">
                 <h2>Add your Notes</h2>
-                <form autoComplete="off" onSubmit={handleclick}>
+                <form autoComplete="off">
                     <div className="mb-3" className="addnoteinput">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" name = "title" id="title"  aria-describedby="emailHelp" onChange={onchange} minLength="3"/>
+                        <input type="text" className="form-control" name = "title" id="title"  aria-describedby="emailHelp" onChange={onchange} minLength="3" placeholder="Enter atleast 3 characters"/>
                     </div>
                     <div className="mb-3" className="addnoteinput">
                         <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
-                        <textarea rows="4" type="text" className="form-control" id="description"  name="description"onChange={onchange} minLength="5" />
+                        <textarea rows="4" type="text" className="form-control" id="description" placeholder="Enter atleast 5 characters" name="description"onChange={onchange} minLength="5" />
                     </div>
                     <div className="mb-3" className="addnoteinput">
                         <label htmlFor="exampleInputPassword1" className="form-label">Tag</label>
                         <input type="text" className="form-control" id="tag"  name="tag"onChange={onchange} />
                     </div>
-                    <button type="submit" id="addnotebtn"  className="btn btn-primary">Add note</button>
+                    <button type="submit" id="addnotebtn" disabled= {note.title.length<3 || note.description.length<5} className="btn btn-primary" onClick={handleclick}>Add note</button>
                 </form>
             </div>
         </>
